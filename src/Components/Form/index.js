55e91bb9a66521e5ed47a4e902462468dd43b691
Julio Neto <julio.neto@gruporojemac.com.br>
2023-02-07 16:32:16 -0300

    02. Trabalhando com props

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,14 +1,36 @@
+import Button from '../Button';
+import Dropdown from '../Dropdown';
 import InputText from '../InputText/';
 import './Form.css';
 
 const Form = () => {
+  const teams = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão',
+  ];
+
+  const onSave = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <section className='form'>
-      <form>
+      <form onSubmit={onSave}>
         <h2>Preencha os dados para criar o card do funcionário</h2>
-        <InputText label='Nome' placeholder='Digite seu nome' />
-        <InputText label='Cargo' placeholder='Digite seu cargo' />
+        <InputText required='true' label='Nome' placeholder='Digite seu nome' />
+        <InputText
+          required='true'
+          label='Cargo'
+          placeholder='Digite seu cargo'
+        />
         <InputText label='Imagem' placeholder='Digite o endereço da imagem' />
+        <Dropdown required='true' label='Time' items={teams} />
+        <Button>Criar Card</Button>
       </form>
     </section>
   );
