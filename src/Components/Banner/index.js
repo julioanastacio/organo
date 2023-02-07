c60966af5c7d1aca38ade1afabb790145ce7e3a7
Julio Neto <julio.neto@gruporojemac.com.br>
2023-02-07 15:53:24 -0300

    01. Uma biblioteca declarativa

---
+++ src/Components/Banner/index.js
@@ -0,0 +1,14 @@
+import './Banner.css';
+
+const Banner = () => {
+  return (
+    <header className='banner'>
+      <img
+        src='/images/banner.png'
+        alt='O banner principal da página do Organo'
+      ></img>
+    </header>
+  );
+};
+
+export default Banner;
